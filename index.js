a4b3af2aac329b42bed58cde5f7ef542f873fcb6
leonbyrdjr <leonbyrdjr@Leons-MBP.hsd1.tx.comcast.net>
2022-08-06 21:12:32 -0500

    commit tree bb2dd4055c75a7d6e91a4f663c7a56fc034217dd

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// }
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+// .then(res => res.json)
+// .then(object => console.log(object))
+
+function submitData(usersName, usersEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept": 'application/json',
+        },
+        body: JSON.stringify({
+            name: usersName,
+            email: usersEmail,
+        })
+    })
+    .then(res => res.json())
+    .then(object => {console.log(object)
+        document.body.innerHTML = object.id
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+}
+
+
+
